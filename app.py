bf7fdd7e38cdaa564f77f35a50cbb68569631f11
nikhil <nikhil4596@gmail.com>
2018-09-08 01:29:38 -0400

    emotions model done

--- app.py
+++ app.py
@@ -1,20 +1,51 @@
 # This file will contain the code required to process the images and thier comparisons
 import cv2
+import math
 import numpy
+import requests
+subscription_key = "526937c01fbc44d6a79439d31e232fbb"
+face_api_url = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect"
+
 
 
 def main():
-    image_name = "face2.jpg"
-    image = cv2.imread(image_name)
-    face = crop_face(image)
-    print(type(face))
-    compute_emotion(face)
+    image1 = "face2.jpg"
+    image2 = "face1.jpg"
+    image1 = cv2.imread(image1)
+    image2 = cv2.imread(image2)
+    face1 = crop_face(image1)
+    face2 = crop_face(image2)
+    score = emotion_score(face1,face2)
+    print(score)
 
+
+def emotion_score(a, b):
+    emotion_a = compute_emotion(a)
+    emotion_b = compute_emotion(b)
+    print(emotion_a)
+    print(emotion_b)
+    score = 0
+    for key in emotion_a.keys():
+        score += math.pow(emotion_a[key] - emotion_b[key], 2)
+    score = math.sqrt(score)
+    print (score)
+    score = ((math.sqrt(2) - score * 1.0) / math.sqrt(2)) * 100
+    return score
 
 def compute_emotion(image):
-    cv2.imshow("face", image)
-    cv2.waitKey(0)
+    # cv2.imshow("face", image)
+    cv2.imwrite("temp_image.jpg", image)
+    # cv2.waitKey(0)
+    image_data = open("temp_image.jpg", "rb").read()
+    params = {'returnFaceAttributes': 'emotion'}
 
+    # image_url = "https://image.shutterstock.com/image-photo/beautiful-face-young-woman-clean-260nw-149962697.jpg"
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key, 'Content-Type': 'application/octet-stream'}
+    # data = {'url': image_url}
+    response = requests.post(face_api_url, params=params, headers=headers, data=image_data)
+    analysis = response.json()
+    emotion = (analysis[0]["faceAttributes"]["emotion"])
+    return emotion
 
 def crop_face(image):
     cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -26,8 +57,6 @@ def crop_face(image):
     x, y, w, h = faces[0]
     # cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 3)
     crop_image = image[y:y + h, x:x + w]
-    # cv2.imshow('face', image)
-    # cv2.imshow("cropped", crop_image)
     return crop_image
 
 
