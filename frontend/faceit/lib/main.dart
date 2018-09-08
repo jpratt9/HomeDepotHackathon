c71c9ba9817f6fd4e08f4a8e8530cc895df33f7a
hsangwan3 <hardiksangwan@lawn-128-61-34-47.lawn.gatech.edu>
2018-09-08 04:11:56 -0400

    Working on FrontEnd

--- frontend/faceit/lib/main.dart
+++ frontend/faceit/lib/main.dart
@@ -88,6 +88,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
+
           children: <Widget>[
             new Text(
               'You have pushed the button this many times:',
